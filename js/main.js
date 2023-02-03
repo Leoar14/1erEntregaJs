cb5669dba0acdb0ef67c806036a9a070bfcaeb48
leonardo del barrio <leo.1905cai@gmail.com>
2023-02-02 23:51:45 -0300

    3er preentrega

--- js/main.js
+++ js/main.js
@@ -8,90 +8,103 @@ const saludo = ()=>{
     }
 saludo()
 
-function showProps(obj) {
-    let result = ``;
+const productos = [
+    {
+        id: 1,
+        nombre: 'Camiseta de Argentina',
+        precio: 24000,
+        descripcion: 'Camiseta original de Argentina para adultos',
+        imagen: './img/camiseta.jpg',
+        categoria: 'Indumentaria',
+        unidades: 10
+    },
+    {
+        id: 2,
+        nombre: 'Conjunto Invierno',
+        precio: 32000,
+        descripcion: 'Conjunto abrigado adultos',
+        imagen: './img/conjunto-largo.jpg',
+        categoria: 'Indumentaria',
+        unidades: 8
+    },
+    {
+        id: 3,
+        nombre: 'Botinero',
+        precio: 13000,
+        descripcion: 'Botinero mediano',
+        imagen: './img/botinero.jpg',
+        categoria: 'Artículo',
+        unidades: 4
+    },
+    {
+        id: 4,
+        nombre: 'Pantalón',
+        precio: 10000,
+        descripcion: 'Pantalon titular de Argentina',
+        imagen: './img/pantalon.jpg',
+        categoria: 'Indumentaria',
+        unidades: 5
+    },
+    {
+        id: 5,
+        nombre: 'Mochila',
+        precio: 18000,
+        descripcion: 'Mochila AFA informal',
+        imagen: './img/mochila.jpg',
+        categoria: 'Artículo',
+        unidades: 15
+    },
+    {
+        id: 6,
+        nombre: 'Gorra',
+        precio: 5000,
+        descripcion: 'Gorras todo los talles',
+        imagen: './img/gorra.jpg',
+        categoria: 'Artículo',
+        unidades: 20
+    },
+]
 
-    for (let i in obj) {
-      // obj.hasOwnProperty() se usa para filtrar propiedades de la cadena de prototipos del objeto
-        if (obj.hasOwnProperty(i)) {
-            result += `\n${i}:\n\tprecio por unidad: $${obj[i]["precio_x_unidad"]}.\n\tcantidad: ${obj[i]["unidades"]}.\n\tsubtotal: $${obj[i]["subtotal"]}\n\n`;
+const contenedorProductos = document.getElementById('contenedor-productos');
+const carritoTexto = document.getElementById('carrito');
+const carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+const nuevaCantidadCarrito = carrito.reduce((acc, { cantidad }) => acc + cantidad, 0);
+    carritoTexto.innerHTML = `Carrito(${nuevaCantidadCarrito})`;
+const agregarTarjetaDeProducto = producto => {const tarjetaProducto = document.createElement('div');
+    tarjetaProducto.className = ' shadow p-3 mb-5 bg-body-tertiary rounded';
+    tarjetaProducto.innerHTML = `
+        <div id="producto-${producto.id}" class="tarjeta ">
+            <img src="${producto.imagen}" class="tarjeta__img">
+            <div class="tarjeta cuerpo">
+                <h4>${producto.nombre}</h4>
+                <p>${producto.descripcion}</p>
+                <p>Categoria: ${producto.categoria}</p> 
+
+                <p  class="tarjeta__precio">$ ${producto.precio}</p>
+                <p>stock: ${producto.unidades}</p>
+                <button class="btn btn-warning">Agregar al carrito</button>
+            </div>
+        </div>`;
+    contenedorProductos.append(tarjetaProducto);
+    document.querySelector(`#producto-${producto.id} button`).addEventListener('click',() => {
+        const productoEnElCarrito = carrito.find((productoCarrito) => productoCarrito.id === producto.id);
+        const cantidad = (productoEnElCarrito?.cantidad || 0) + 1
+        if (productoEnElCarrito) {productoEnElCarrito.cantidad = cantidad;
+        } else {carrito.push({...producto,cantidad});
         }
-    }
-    return result;
+
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+    const nuevaCantidadCarrito = carrito.reduce((acc, { cantidad }) => acc + cantidad, 0);
+    carritoTexto.innerHTML = `Carrito(${nuevaCantidadCarrito})`;
+    });
 }
-
-let carritoDeCompras = {};
-let carritoFinal = [];
-let productos = {
-    "camiseta": {
-        "precio": 24000,
-        "cantidad": 10
-    },
-    "pantalon": {
-        "precio": 10000,
-        "cantidad": 10
-    },
-    "gorra": {
-        "precio": 5000,
-        "cantidad": 100
-    },
-    "mochila": {
-        "precio": 18000,
-        "cantidad": 5
+productos.forEach((producto => {agregarTarjetaDeProducto(producto);}));
+document.getElementById('select-categoria').addEventListener('input',
+    (e) => {const categoriaAFiltrar = e.target.value;   
+        const productosFiltrados = categoriaAFiltrar ?productos.filter((producto) => producto.categoria === categoriaAFiltrar) :productos;
+        document.querySelectorAll('.col').forEach(child => child.remove());
+        productosFiltrados.forEach(producto => {
+        agregarTarjetaDeProducto(producto);
+        });
     }
-};
-let keys = Object.keys(productos);
-
-
-let total = 0;
-
-const elegirProducto = () => {
-
-    let unidades = 0;
-    let subtotal = 0;
-
-    do{
-        productoElegido = prompt("Ingrese el nombre sin mayusculas ni tilde de la prenda a consultar: \n"+
-                            `\t camiseta => precio: $${productos["camiseta"]["precio"]} => cantidad disp.: ${productos["camiseta"]["cantidad"]} \n`+
-                            `\t pantalon => precio: $$productost["pantalon"]["precio"]} => cantidad disp.: ${productos["pantalon"]["cantidad"]} \n`+
-                            `\t gorra => precio: $${productos["gorra"]["precio"]} => cantidad disp.: ${productos["gorra"]["cantidad"]} \n`+
-                            `\t mochila => precio: $${productos["mochila"]["precio"]} => cantidad disp.: ${productos["mochila"]["cantidad"]} \n`+
-                            "\t Ingrese NO, si no desea comprar nada");
-        if(keys.includes(productoElegido) || productoElegido === "NO"){
-            do{
-                unidades = parseInt(prompt("¿Cuantas unidades quiere lleconst?\n"+
-                `\t ${productoElegido} => precio: $${productos[productoElegido]["precio"]} => cantidad disp.: ${productos[productoElegido]["cantidad"]} \n`));
-                if(unidades < 0 || unidades > productos[productoElegido]["cantidad"]) {
-                    alert(`Debe ingresar un Numero mayor a 0 y menor o igual a ${productos[productoElegido]["cantidad"]}`)
-                }
-            }while(unidades < 0 || unidades > productos[productoElegido]["cantidad"])
-            subtotal = productos[productoElegido]["precio"] * unidades
-                    carritoDeCompras[productoElegido] = {
-                        "precio_x_unidad": productos[productoElegido]["precio"],
-                        "unidades": unidades,
-                        "subtotal": subtotal
-                    };
-                    // Falta restar las unidades al stock
-                    result =showProps(carritoDeCompras)
-                    alert ("Su carrito hasta el momento es: \n"+`\t* ${result}`)
-                    productoElegido = prompt("Desea seguir comprando?: \n"+
-                                        `\t* SI \n`+
-                                        `\t* NO \n`)
-        } else {
-            alert(`No tenemos este producto: ${productoElegido}`)
-        }
-    }while(productoElegido != "NO")
-
-    if (productoElegido == "NO" && Object.entries(carritoDeCompras).length === 0){
-        alert("gracias por su visita. Que tenga un buen dia")
-    }
-
-    if (Object.entries(carritoDeCompras).length != 0){
-        for (const product in carritoDeCompras) {
-            total += carritoDeCompras[product]["subtotal"]
-        }
-        alert(`Total a Pagar: $${total}`);
-    }
-}
-
-elegirProducto()
+);
