ee82c7bfaf901d8c93fbc9254d83821fe81cc6ea
leonardo del barrio <leo.1905cai@gmail.com>
2023-02-19 23:38:31 -0300

    entrega final js

--- js/main.js
+++ js/main.js
@@ -1,110 +1,106 @@
-const saludo = ()=>{
-    const nombre = prompt ("Ingrese nombre");
-    if(nombre != ""){
-    alert (`Bienvenido ${nombre}`)
-    }else {
-        alert("No ingresaste tu nombre")
-    }
-    }
-saludo()
+let productos = [];
 
-const productos = [
-    {
-        id: 1,
-        nombre: 'Camiseta de Argentina',
-        precio: 24000,
-        descripcion: 'Camiseta original de Argentina para adultos',
-        imagen: './img/camiseta.jpg',
-        categoria: 'Indumentaria',
-        unidades: 10
-    },
-    {
-        id: 2,
-        nombre: 'Conjunto Invierno',
-        precio: 32000,
-        descripcion: 'Conjunto abrigado adultos',
-        imagen: './img/conjunto-largo.jpg',
-        categoria: 'Indumentaria',
-        unidades: 8
-    },
-    {
-        id: 3,
-        nombre: 'Botinero',
-        precio: 13000,
-        descripcion: 'Botinero mediano',
-        imagen: './img/botinero.jpg',
-        categoria: 'Artículo',
-        unidades: 4
-    },
-    {
-        id: 4,
-        nombre: 'Pantalón',
-        precio: 10000,
-        descripcion: 'Pantalon titular de Argentina',
-        imagen: './img/pantalon.jpg',
-        categoria: 'Indumentaria',
-        unidades: 5
-    },
-    {
-        id: 5,
-        nombre: 'Mochila',
-        precio: 18000,
-        descripcion: 'Mochila AFA informal',
-        imagen: './img/mochila.jpg',
-        categoria: 'Artículo',
-        unidades: 15
-    },
-    {
-        id: 6,
-        nombre: 'Gorra',
-        precio: 5000,
-        descripcion: 'Gorras todo los talles',
-        imagen: './img/gorra.jpg',
-        categoria: 'Artículo',
-        unidades: 20
-    },
-]
+fetch("./js/productos.json")
+    .then(response => response.json())
+    .then(data => {
+        productos = data;
+        cargarProductos(productos);
+    })
 
-const contenedorProductos = document.getElementById('contenedor-productos');
-const carritoTexto = document.getElementById('carrito');
-const carrito = JSON.parse(localStorage.getItem('carrito')) || [];
-const nuevaCantidadCarrito = carrito.reduce((acc, { cantidad }) => acc + cantidad, 0);
-    carritoTexto.innerHTML = `Carrito(${nuevaCantidadCarrito})`;
-const agregarTarjetaDeProducto = producto => {const tarjetaProducto = document.createElement('div');
-    tarjetaProducto.className = ' shadow p-3 mb-5 bg-body-tertiary rounded';
-    tarjetaProducto.innerHTML = `
-        <div id="producto-${producto.id}" class="tarjeta ">
-            <img src="${producto.imagen}" class="tarjeta__img">
-            <div class="tarjeta cuerpo">
-                <h4>${producto.nombre}</h4>
-                <p>${producto.descripcion}</p>
-                <p>Categoria: ${producto.categoria}</p> 
+const contenedorProductos = document.querySelector("#contenedor-productos");
+const botonesCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector("#titulo-principal");
+let botonesAgregar = document.querySelectorAll(".producto-agregar");
+const numerito = document.querySelector("#numerito");
 
-                <p  class="tarjeta__precio">$ ${producto.precio}</p>
-                <p>stock: ${producto.unidades}</p>
-                <button class="btn btn-warning">Agregar al carrito</button>
-            </div>
-        </div>`;
-    contenedorProductos.append(tarjetaProducto);
-    document.querySelector(`#producto-${producto.id} button`).addEventListener('click',() => {
-        const productoEnElCarrito = carrito.find((productoCarrito) => productoCarrito.id === producto.id);
-        const cantidad = (productoEnElCarrito?.cantidad || 0) + 1
-        if (productoEnElCarrito) {productoEnElCarrito.cantidad = cantidad;
-        } else {carrito.push({...producto,cantidad});
+botonesCategorias.forEach(boton => boton.addEventListener("click", () => {
+    aside.classList.remove("aside-visible");
+}))
+
+function cargarProductos(productosElegidos) {
+    contenedorProductos.innerHTML = "";
+    productosElegidos.forEach(producto => {
+        const div = document.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML = `
+            <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+            <div class="producto-detalles">
+                <h3 class="producto-titulo">${producto.titulo}</h3>
+                <p class="producto-precio">$${producto.precio}</p>
+                <button class="producto-agregar" id="${producto.id}">Agregar</button>
+            </div>`;
+        contenedorProductos.append(div);
+    })
+    actualizarBotonesAgregar();
+}
+
+botonesCategorias.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+        botonesCategorias.forEach(boton => boton.classList.remove("active"));
+        e.currentTarget.classList.add("active");
+        if (e.currentTarget.id != "todos") {
+            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+            cargarProductos(productosBoton);
+        } else {
+            tituloPrincipal.innerText = "Todos los productos";
+            cargarProductos(productos);
         }
+    })
+});
 
-    localStorage.setItem('carrito', JSON.stringify(carrito));
-    const nuevaCantidadCarrito = carrito.reduce((acc, { cantidad }) => acc + cantidad, 0);
-    carritoTexto.innerHTML = `Carrito(${nuevaCantidadCarrito})`;
+function actualizarBotonesAgregar() {
+    botonesAgregar = document.querySelectorAll(".producto-agregar");
+    botonesAgregar.forEach(boton => {
+        boton.addEventListener("click", agregarAlCarrito);
     });
 }
-productos.forEach((producto => {agregarTarjetaDeProducto(producto);}));
-document.getElementById('select-categoria').addEventListener('input',
-    (e) => {const categoriaAFiltrar = e.target.value;   
-        const productosFiltrados = categoriaAFiltrar ?productos.filter((producto) => producto.categoria === categoriaAFiltrar) :productos;
-        document.querySelectorAll('.col').forEach(child => child.remove());
-        productosFiltrados.forEach(producto => {
-        agregarTarjetaDeProducto(producto);
-        });
+
+let productosEnCarrito;
+let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+if (productosEnCarritoLS) {
+    productosEnCarrito = JSON.parse(productosEnCarritoLS);
+    actualizarNumerito();
+} else {
+    productosEnCarrito = [];
+}
+
+function agregarAlCarrito(e) {
+    Toastify({
+        text: "Producto agregado",
+        duration: 3000,
+        close: true,
+        gravity: "top",
+        position: "right",
+        stopOnFocus: true,
+        style: {
+            background: "linear-gradient(to right, #4b33a8, #785ce9)",
+            borderRadius: "2rem",
+            textTransform: "uppercase",
+            fontSize: ".75rem"
+        },
+        offset: {
+            x: '1.5rem', // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+            y: '1.5rem' // vertical axis - can be a number or a string indicating unity. eg: '2em'
+            },
+        onClick: function(){} // Callback after click
+        }).showToast();
+
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id === idBoton);
+    if(productosEnCarrito.some(producto => producto.id === idBoton)) {
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+        productosEnCarrito[index].cantidad++;
+    } else {
+        productoAgregado.cantidad = 1;
+        productosEnCarrito.push(productoAgregado);
     }
-);
+    actualizarNumerito();
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+}
+
+function actualizarNumerito() {
+    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numerito.innerText = nuevoNumerito;
+}
